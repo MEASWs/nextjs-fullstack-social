f04a1fceae2eee1724c80e062b14a35fe77b323b
MEASWs <mookvarin.cc1976@gmail.com>
2025-04-21 19:23:12 +0700

    project completed

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TasksPage() {
+    console.log("hi tasks")
+    return (
+        <div>
+            tasks
+        </div>
+    )
+}
+
+export default TasksPage
